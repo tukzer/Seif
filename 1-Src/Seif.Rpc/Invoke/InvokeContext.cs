da48d8ca341d0cbceec028e8ea8172af824753b6
tukzer <tukzer@hotmail.com>
2017-02-22 16:21:52 +0800

    test

---
+++ 1-Src/Seif.Rpc/Invoke/InvokeContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seif.Rpc.Invoke
+{
+    public class InvokeContext
+    {
+        public IInvocation Invocation { get; set; }
+        public InvokeResult InvokeResult { get; set; }
+        public Exception InvokeException { get; set; }
+        public bool IsExceptionHandled { get; set; }
+    }
+}
---
+++ Seif.Calc/Lexer.cs
@@ -0,0 +1,17 @@
+﻿namespace Seif.Calc
+{
+    public class Lexer
+    {
+        private readonly string _source;
+
+        public Lexer(string source)
+        {
+            _source = source;
+        }
+
+        protected void Analyze()
+        {
+            
+        }
+    }
+}
